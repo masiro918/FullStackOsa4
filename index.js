ed37107fb73c18e7e410b95bad1ec5bd76739694
masiro918 <matias.siro@outlook.com>
2025-11-03 14:27:37 +0200

    Tehtävä 4.1

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(express.json())
